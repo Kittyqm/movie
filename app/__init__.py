32e3371e9c2f06de8c51b44024cb172ae0c98c66
邹方成 <aa931912343@qq.com>
2017-10-16 16:57:12 +0800

    会员登陆

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:mysql@127.0.0.1/movies"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = "3021d87718e043f49dad499fe3494a9d"
 app.config['UP_DIR'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads/')
+app.config['FC_DIR'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads/users')
 app.debug = True
 db = SQLAlchemy(app)
 
@@ -20,6 +21,10 @@ from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+# 微信配置
+WECHAT_WEBSITE_APP_ID = "wx9d2b3cec1b25d4a4"
+WECHAT_WEBSITE_APP_SECRET = "80cc447f0fb17b7b74733666b99ab3b6"
 
 
 @app.errorhandler(404)
